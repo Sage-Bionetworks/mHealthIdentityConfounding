ec200b94f92df916c458b3c0b78fb5eca1f285f9
echaibub <chaibubneto@gmail.com>
2017-09-22 15:54:47 -0700

    initial code

---
+++ code/simulated_example_1.R
@@ -0,0 +1,141 @@
+
+source("utility_functions_for_disease_recognition_and_individual_confounding_tests.R")
+
+nFeatures <- 10 
+nRecordsRange <- c(10, 20) 
+nCases <- 13
+nControls <- 7
+
+nperm <- 1e+4
+
+#####################################
+## example 1  
+#####################################
+
+set.seed(12345001)
+mu1 <- rnorm(nCases + nControls, 0, 0)
+dat1 <- SimulateData(nFeatures, 
+                     nRecordsRange, 
+                     nCases,
+                     nControls,
+                     mu = mu1,
+                     a = 0, 
+                     b = 2, 
+                     c = 1, 
+                     d = 0.5,
+                     rhoRecord = 0.95,
+                     rhoFeature = 0.5)
+subjectSplit1 <- GetIdxTrainTestSplitBySubject(dat = dat1, 
+                                               nSplits = 2, 
+                                               subjectIdName = "subjectId",
+                                               labelName = "diseaseLabel",
+                                               negClassName = "-1", 
+                                               posClassName = "1")
+recordSplit1 <- GetIdxTrainTestSplitByRecord(dat1, nSplits = 2)
+
+
+aucR1 <- GetAUC(dat = dat1, 
+                idxTrain = recordSplit1$idxTrain, 
+                idxTest = recordSplit1$idxTest, 
+                subjectIdName = "subjectId", 
+                labelName = "diseaseLabel", 
+                featNames = paste("feature", seq(10), sep = ""),
+                negClassName = "-1", 
+                posClassName = "1")
+aucR1$aucObs
+
+aucS1 <- GetAUC(dat = dat1, 
+                idxTrain = subjectSplit1$idxTrain, 
+                idxTest = subjectSplit1$idxTest, 
+                subjectIdName = "subjectId", 
+                labelName = "diseaseLabel", 
+                featNames = paste("feature", seq(10), sep = ""),
+                negClassName = "-1", 
+                posClassName = "1")
+aucS1$aucObs
+
+
+plot(aucR1$rocObj)
+plot(aucS1$rocObj, add = TRUE, lty = 2)
+
+
+#########################################
+## record-wise data split
+#########################################
+
+set.seed(12345001)
+cat("record-wise split 1", "\n")
+dr.rws.1 <- DRPermDistrAUC(dat = dat1, 
+                           idxTrain = recordSplit1$idxTrain, 
+                           idxTest = recordSplit1$idxTest, 
+                           nperm = nperm, 
+                           subjectIdName = "subjectId", 
+                           labelName = "diseaseLabel", 
+                           featNames = paste("feature", seq(10), sep = ""),
+                           negClassName = "-1", 
+                           posClassName = "1",
+                           verbose = TRUE)
+
+## nperm hardcoded
+set.seed(12345001)
+cat("record-wise split 1", "\n")
+ic.rws.1 <- ICPermDistrAUC(dat = dat1, 
+                           idxTrain = recordSplit1$idxTrain, 
+                           idxTest = recordSplit1$idxTest, 
+                           npermf = 1000,
+                           nperml = 30,
+                           subjectIdName = "subjectId", 
+                           labelName = "diseaseLabel", 
+                           featNames = paste("feature", seq(10), sep = ""),
+                           negClassName = "-1", 
+                           posClassName = "1",
+                           verbose = TRUE)
+
+
+#########################################
+## subject-wise data split
+#########################################
+
+set.seed(12345001)
+cat("record-wise split 1", "\n")
+dr.sws.1 <- DRPermDistrAUC(dat = dat1, 
+                           idxTrain = subjectSplit1$idxTrain, 
+                           idxTest = subjectSplit1$idxTest, 
+                           nperm = nperm, 
+                           subjectIdName = "subjectId", 
+                           labelName = "diseaseLabel", 
+                           featNames = paste("feature", seq(10), sep = ""),
+                           negClassName = "-1", 
+                           posClassName = "1",
+                           verbose = TRUE)
+
+set.seed(12345001)
+cat("record-wise split 1", "\n")
+ic.sws.1 <- ICPermDistrAUC(dat = dat1, 
+                           idxTrain = subjectSplit1$idxTrain, 
+                           idxTest = subjectSplit1$idxTest, 
+                           npermf = 1000,
+                           nperml = 30,
+                           subjectIdName = "subjectId", 
+                           labelName = "diseaseLabel", 
+                           featNames = paste("feature", seq(10), sep = ""),
+                           negClassName = "-1", 
+                           posClassName = "1",
+                           verbose = TRUE)
+
+
+######################################################
+######################################################
+######################################################
+
+nc <- 10
+hist(dr.rws.1, probability = TRUE, nclass = nc)
+abline(v = aucR1$aucObs)
+sum(dr.rws.1 >= aucR1$aucObs)/length(dr.rws.1)
+
+#hist(ic.rws.1, probability = TRUE, nclass = nc)
+
+hist(dr.sws.1, probability = TRUE, nclass = nc)
+abline(v = aucS1$aucObs)
+
+sum(dr.sws.1 >= aucS1$aucObs)/length(dr.sws.1)
---
+++ code/utility_functions_for_disease_recognition_and_individual_confounding_tests.R
@@ -0,0 +1,396 @@
+
+library(pROC)
+library(randomForest)
+library(MatchIt)
+
+
+####################################
+## analysis functions
+####################################
+
+SubjectWiseLabelShuffling0 <- function(dat, subjectIdName, labelName) {
+  ids <- as.character(unique(dat[, subjectIdName]))
+  nids <- length(ids)
+  dat[, labelName] <- as.character(dat[, labelName])
+  labels <- dat[match(ids, dat[, subjectIdName]), labelName]
+  slabels <- labels[sample(nids)]
+  for (i in seq(nids)) {
+    dat[which(dat[, subjectIdName] == ids[i]), labelName] <- slabels[i]
+  }
+  dat[, labelName] <- as.factor(dat[, labelName])
+  
+  dat
+}
+
+
+## This function makes sure that we have both labels in the training and test sets.
+## It keeps generating new shuffles of the label data until both labels are present
+## in the training and test sets.
+## 
+SubjectWiseLabelShuffling <- function(dat, subjectIdName, labelName, idxTrain, idxTest) {
+  aux <- 0
+  while (aux == 0) {
+    datS <- SubjectWiseLabelShuffling0(dat, subjectIdName, labelName)
+    labelsTrain <- unique(datS[idxTrain, labelName])
+    labelsTest <- unique(datS[idxTest, labelName])
+    if (length(labelsTrain) == 2 & length(labelsTest) == 2) {
+      aux <- 1
+    }
+  }
+  
+  datS
+}
+
+
+DRPermDistrAUC <- function(dat, 
+                           idxTrain,
+                           idxTest,
+                           nperm, 
+                           subjectIdName, 
+                           labelName, 
+                           featNames,
+                           negClassName, 
+                           posClassName,
+                           verbose = TRUE) {
+  dat <- dat[, c(labelName, featNames, subjectIdName)]
+  dat[, labelName] <- factor(as.character(dat[, labelName]), 
+                             levels = c(negClassName, posClassName)) 
+  myFormula <- as.formula(paste(labelName, " ~ ", paste(featNames, collapse = " + ")))
+  nullDistr <- rep(NA, nperm)
+  if (verbose) {
+    for (i in seq(nperm)) {
+      cat(i, "\n") 
+      datPS <- SubjectWiseLabelShuffling(dat, subjectIdName, labelName, idxTrain, idxTest)
+      fitPS <- randomForest(myFormula, data = datPS[idxTrain,])
+      predProbsPS <- predict(fitPS, datPS[idxTest, -1, drop = FALSE], type = "prob")
+      rocObjPS <- roc(datPS[idxTest, 1], predProbsPS[, posClassName], direction = "<", 
+                      levels = c(negClassName, posClassName)) 
+      nullDistr[i] <- pROC::auc(rocObjPS)[1]
+    }
+  }
+  else {
+    for (i in seq(nperm)) {
+      datPS <- SubjectWiseLabelShuffling(dat, subjectIdName, labelName, idxTrain, idxTest)
+      fitPS <- randomForest(myFormula, data = datPS[idxTrain,])
+      predProbsPS <- predict(fitPS, datPS[idxTest, -1, drop = FALSE], type = "prob")
+      rocObjPS <- roc(datPS[idxTest, 1], predProbsPS[, posClassName], direction = "<", 
+                      levels = c(negClassName, posClassName)) 
+      nullDistr[i] <- pROC::auc(rocObjPS)[1]
+    } 
+  }
+  
+
+  
+  nullDistr 
+}
+
+
+
+
+RecordWiseFeatureShuffling <- function(dat, featNames) {
+  n <- nrow(dat)
+  dat[, featNames] <- dat[sample(n), featNames]
+  
+  dat
+}
+
+
+ICPermDistrAUC <- function(dat, 
+                           idxTrain,
+                           idxTest,
+                           npermf,
+                           nperml,
+                           subjectIdName, 
+                           labelName, 
+                           featNames,
+                           negClassName, 
+                           posClassName,
+                           verbose = TRUE) {
+  dat <- dat[, c(labelName, featNames, subjectIdName)]
+  dat[, labelName] <- factor(as.character(dat[, labelName]), 
+                             levels = c(negClassName, posClassName)) 
+  myFormula <- as.formula(paste(labelName, " ~ ", paste(featNames, collapse = " + ")))
+  nullDistr <- rep(NA, nperm)
+  if (verbose) {
+    for (i in seq(npermf)) {
+      cat(i, "\n")
+      datS <- RecordWiseFeatureShuffling(dat, featNames)
+      aux <- DRPermDistrAUC(datS, 
+                            idxTrain,
+                            idxTest,
+                            nperm = nperml, 
+                            subjectIdName, 
+                            labelName, 
+                            featNames,
+                            negClassName, 
+                            posClassName,
+                            verbose = FALSE)
+      nullDistr[i] <- median(aux)
+    }
+  }
+  else {
+    for (i in seq(npermf)) {
+      datS <- RecordWiseFeatureShuffling(dat, featNames)
+      aux <- DRPermDistrAUC(datS, 
+                            idxTrain,
+                            idxTest,
+                            nperm = nperml, 
+                            subjectIdName, 
+                            labelName, 
+                            featNames,
+                            negClassName, 
+                            posClassName,
+                            verbose = FALSE)
+      nullDistr[i] <- median(aux)
+    }
+  }
+  
+  nullDistr
+}
+
+
+GetAUC <- function(dat,
+                   idxTrain, 
+                   idxTest, 
+                   subjectIdName, 
+                   labelName, 
+                   featNames,
+                   negClassName, 
+                   posClassName) {
+  dat <- dat[, c(labelName, featNames, subjectIdName)]
+  dat[, labelName] <- factor(as.character(dat[, labelName]), 
+                             levels = c(negClassName, posClassName)) 
+  myFormula <- as.formula(paste(labelName, " ~ ", paste(featNames, collapse = " + ")))
+  fit <- randomForest(myFormula, data = dat[idxTrain,])
+  predProbs <- predict(fit, dat[idxTest, -1, drop = FALSE], type = "prob")
+  rocObj <- roc(dat[idxTest, 1], predProbs[, posClassName], direction = "<", 
+                levels = c(negClassName, posClassName))    
+  aucObs <- pROC::auc(rocObj)[1]
+  
+  list(aucObs = aucObs, rocObj = rocObj)
+}
+
+
+GetIdxTrainTestSplitBySubject <- function(dat, 
+                                          nSplits = 2, 
+                                          subjectIdName,
+                                          labelName,
+                                          negClassName = "FALSE", 
+                                          posClassName = "TRUE") {
+  ids <- as.character(unique(dat[, subjectIdName]))
+  labels <- dat[match(ids, dat[, subjectIdName]), labelName]
+  caseIds <- ids[which(labels == posClassName)]
+  controlIds <- ids[which(labels == negClassName)]
+  nCase <- length(caseIds)
+  nControl <- length(controlIds)  
+  testControlIds <- sample(controlIds, round(nControl/nSplits), replace = FALSE)
+  trainControlIds <- setdiff(controlIds, testControlIds)
+  testCaseIds <- sample(caseIds, round(nCase/nSplits), replace = FALSE)
+  trainCaseIds <- setdiff(caseIds, testCaseIds) 
+  trainIds <- c(trainControlIds, trainCaseIds)
+  testIds <- c(testControlIds, testCaseIds) 
+  idxTrain <- dat[, subjectIdName] %in% trainIds
+  idxTest <- dat[, subjectIdName] %in% testIds
+  
+  list(idxTrain = idxTrain, idxTest = idxTest)
+}
+
+
+GetIdxTrainTestSplitByRecord <- function(dat, nSplits = 2) {
+  n <- nrow(dat)
+  idxTrain <- sample(n, round(n/2), replace = FALSE)
+  idxTest <- setdiff(seq(n), idxTrain)
+  
+  list(idxTrain = idxTrain, idxTest = idxTest)
+}
+
+
+
+
+########################################
+## data simulation functions
+########################################
+
+
+SimulateData <- function(nFeatures, 
+                         nRecordsRange, 
+                         nCases,
+                         nControls,
+                         mu,
+                         a, 
+                         b, 
+                         c, 
+                         d, 
+                         rhoRecord, 
+                         rhoFeature) {
+  # n: nFeatures
+  # r: nRecords
+  # s: nSubjects
+  # y_s = beta_s
+  Psi <- CreateCorrelationMatrixForFeatures(rho = rhoFeature, p = nFeatures)
+  nRecords <- sample(seq(nRecordsRange[1], nRecordsRange[2], by = 1), 1)
+  Sigma <- CreateCorrelationMatrixForRecords(rho = rhoRecord, p = nRecords)
+  M <- matrix(0, nRecords, nFeatures)
+  I_r <- diag(nRecords)
+  I_n <- diag(nFeatures)
+  nSubjects <- nCases + nControls
+  y_s <- c(rep(-1, nControls), rep(1, nCases))[sample(nSubjects)]
+  ## generate dat for subject 1
+  U_s <- GenerateMatrixNormalErrorMatrix(M, Sigma, I_n)
+  V_s <- GenerateMatrixNormalErrorMatrix(M, I_r, I_n)
+  E_s <- GenerateMatrixNormalErrorMatrix(M, I_r, Psi)
+  feat <- mu[1] + a * y_s[1] + b * U_s + c * V_s + d * E_s
+  diseaseLabel <- rep(y_s[1], nRecords)
+  subjectId <- rep(1, nRecords)
+  ## generate dat for other subjects
+  for (i in 2:nSubjects) {
+    nRecords <- sample(seq(nRecordsRange[1], nRecordsRange[2], by = 1), 1)
+    Sigma <- CreateCorrelationMatrixForRecords(rho = rhoRecord, p = nRecords)
+    M <- matrix(0, nRecords, nFeatures)
+    I_r <- diag(nRecords)
+    U_s <- GenerateMatrixNormalErrorMatrix(M, Sigma, I_n)
+    V_s <- GenerateMatrixNormalErrorMatrix(M, I_r, I_n)
+    E_s <- GenerateMatrixNormalErrorMatrix(M, I_r, Psi)    
+    aux <- mu[i] + a * y_s[i] + b * U_s + c * V_s + d * E_s
+    feat <- rbind(feat, aux)
+    diseaseLabel <- c(diseaseLabel, rep(y_s[i], nRecords))
+    subjectId <- c(subjectId, rep(i, nRecords))
+  }
+  subjectId <- paste("subject", subjectId, sep = "")
+  colnames(feat) <- paste("feature", seq(nFeatures), sep = "")
+  diseaseLabel <- as.factor(diseaseLabel)
+  dat <- data.frame(subjectId, diseaseLabel, feat)
+  
+  dat
+}
+
+
+GenerateMatrixNormalErrorMatrix <- function(M, Sigma, Psi) {
+  ## M: mean matrix (R by N)
+  ## Sigma: covariance across rows (R by R)
+  ## Psi: covariance across columns (N by N)
+  r <- nrow(M)
+  n <- ncol(M)
+  Z <- matrix(rnorm(r * n), r, n)
+  cholSigma <- chol(Sigma)
+  cholPsi <- chol(Psi)
+  E <- M + cholSigma %*% Z %*% cholPsi
+  
+  E
+}
+
+
+CreateCorrelationMatrixForRecords <- function(rho, p) {
+  aux1 <- matrix(rep(1:p, p), p, p)
+  aux2 <- matrix(rep(1:p, each = p), p, p) 
+  rho^abs(aux1 - aux2)
+}
+
+
+CreateCorrelationMatrixForFeatures <- function(rho, p) {
+  Psi <- matrix(rho, p, p)
+  diag(Psi) <- 1
+  
+  Psi
+}
+
+
+######################################
+## mPower specific functions
+######################################
+
+
+FilterOutParticipantsWithFewRecords <- function(dat, thr) {
+  aux <- table(dat$healthCode)
+  aux <- sort(aux, decreasing = TRUE)
+  keep <- names(aux)[which(aux >= thr)]
+  
+  dat[dat$healthCode %in% keep,]
+}
+
+
+MyMatching <- function(dat) {
+  rownames(dat) <- dat$healthCode
+  
+  ## convert education to numeric scores
+  education2 <- rep(NA, nrow(dat))
+  education2[dat$education == "High School Diploma/GED"] <- 1
+  education2[dat$education == "Some college"] <- 2
+  education2[dat$education == "2-year college degree"] <- 2
+  education2[dat$education == "4-year college degree"] <- 3
+  education2[dat$education == "Some graduate school"] <- 4
+  education2[dat$education == "Master's Degree"] <- 5
+  education2[dat$education == "Doctoral Degree"] <- 6
+  dat$education2 <- education2
+  
+  ## get the negation of the professional.diagnosis variable
+  ## needed because matchit with "nearest" assumes data we match
+  ## a "treatment" to several "controls", but we have a single control
+  ## and several cases
+  #dat$isControl <- !dat$professional.diagnosis
+  
+  dat <- dat[, c("professional.diagnosis", "age", "education2")]
+  dat <- na.omit(dat)
+  
+  ## exact matching by age
+  m1 <- matchit(professional.diagnosis ~ age, data = dat, method = "exact")
+  mdat1 <- match.data(m1)
+  aux1 <- table(mdat1$subclass)
+  one2one <- names(aux1)[which(aux1 == 2)]
+  mdat <- mdat1[mdat1$subclass %in% one2one, c("professional.diagnosis", "age", "education2")]
+  
+  ## perform nearest match to controls with multiple age matched cases
+  one2many <- setdiff(names(aux1), one2one)
+  n <- length(one2many)
+  for (i in seq(n)) {
+    sdat <- mdat1[mdat1$subclass == one2many[i], c("professional.diagnosis", "age", "education2")]
+    nCases <- sum(sdat$professional.diagnosis == TRUE)
+    nControls <- sum(sdat$professional.diagnosis == FALSE)
+    if (nCases == nControls | nCases < nControls) {
+      m2 <- matchit(professional.diagnosis ~ education2, data = sdat, method = "nearest")
+    }
+    else {
+      ## the negation of the professional.diagnosis variable is
+      ## needed because matchit with "nearest" assumes data we match
+      ## a "treatment" to several "controls", but we have a single control
+      ## and several cases
+      sdat$isControl <- ! sdat$professional.diagnosis
+      m2 <- matchit(isControl ~ education2, data = sdat, method = "nearest")
+    }
+    mdat2 <- match.data(m2)
+    mdat <- rbind(mdat, mdat2[, c("professional.diagnosis", "age", "education2")])
+  }
+  
+  ## shape output 
+  mdat <- mdat[order(mdat$age),]
+  mdatCases <- mdat[mdat$professional.diagnosis == TRUE,]
+  mdatControls <- mdat[mdat$professional.diagnosis == FALSE,]
+  matchedParticipants <- cbind(rownames(mdatCases), rownames(mdatControls))
+  colnames(matchedParticipants) <- c("case", "control")
+  rownames(matchedParticipants) <- mdatCases$age
+  
+  matchedParticipants
+}
+
+
+## balanced by age and diagnosis
+GetIdxTrainTestSplitBySubjectBalanced <- function(dat, 
+                                                  matchedCaseControlParticipants,
+                                                  subjectIdName = "healthCode",
+                                                  labelName = "professional.diagnosis",
+                                                  negClassName = "FALSE", 
+                                                  posClassName = "TRUE") {
+  
+  ids <- as.character(unique(dat$healthCode))
+  n <- nrow(matchedCaseControlParticipants)
+  nTrain <- ceiling(n/2)
+  idxTrainCases <- sample(seq(n), nTrain, replace = FALSE)
+  idxTrainControls <- setdiff(seq(n), idxTrainCases)
+  trainHealthCodes <- c(matchedCaseControlParticipants[idxTrainCases, "case"], 
+                        matchedCaseControlParticipants[idxTrainControls, "control"])
+  testHealthCodes <- setdiff(ids, trainHealthCodes)
+  idxTrain <- which(dat[, subjectIdName] %in% trainHealthCodes)
+  idxTest <- which(dat[, subjectIdName] %in% testHealthCodes)
+  
+  list(idxTrain = idxTrain, idxTest = idxTest)
+}
